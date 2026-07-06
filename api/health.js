59278b80117ec456e6d972b24cd12aaf1dfa25f2
missnapokita <138368066+missnapokita@users.noreply.github.com>
2026-07-07 00:19:38 +0800

    Delete api directory

--- api/health.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('Cache-Control', 'no-store');
-  return res.status(200).json({ ok: true, name: 'BIDAMAX API', time: new Date().toISOString() });
-};
--- api/series.js
+++
@@ -1,9 +0,0 @@
-const { sendJsonFromGithub } = require('./shared');
-
-module.exports = async function handler(req, res) {
-  return sendJsonFromGithub(req, res, {
-    filePath: 'series.json',
-    cacheSeconds: 600,
-    staleSeconds: 86400
-  });
-};
